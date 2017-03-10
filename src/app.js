3ed89e9ca6eb04f6ae5c4fdd6734c9aee6fa4cfd
m.fedorov <m.fedorov@rivc-pulkovo.ru>
2017-03-10 11:59:47 +0300

    react-practice

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { Header } from './components/index';
+import { DevTools } from './utils/index';
 
 export default class App extends React.Component {
-    static path = '';
-
+    
+    static propTypes = {
+        children: PropTypes.any.isRequired  
+    };
+    static path = '/';
+    
     render() {
         return (
-            <h1>Hello World!</h1>
+            <div>
+                <Header />
+                { this.props.children }
+                { process.env.NODE_ENV !== 'production' ? <DevTools /> : null }
+            </div>
         );
     }
+    
 }
